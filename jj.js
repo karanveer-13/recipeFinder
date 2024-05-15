3a29e262575cd4c6a80a4ca8a1c3b704ef184054
karanveer-13 <159625512+karanveer-13@users.noreply.github.com>
2024-05-15 22:53:51 +0530

    add error in jj.js

--- jj.js
+++ jj.js
@@ -11,7 +11,7 @@ function searchRecipe() {
         console.log(data.meals.length);
         recipeContainer.innerHTML = '';
         recipeButtons.innerHTML = '';
-        if(data.meals.length > 1)
+        if(data.meals.length != 1)
         {
           for(var i = 0;i<data.meals.length; i++)
           {
@@ -24,7 +24,14 @@ function searchRecipe() {
       })
       .catch(error => {
         console.error('Error fetching data:', error);
-        alert('Failed to fetch recipe. Please try again later.');
+        // alert('Failed to fetch recipe. Please try again later.');
+        recipeContainer.innerHTML = 'We do not have this recipe yet 😐'
+        fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(response => response.json())
+        .then(data => {
+          recipeButtons.innerHTML='<h5 id="error_text">Wanna try this: </h5>'
+          displayRecipeButton(data.meals[0]);
+        })
       });
   }
 
@@ -44,6 +51,7 @@ function searchRecipe() {
 
   function displayRecipe(recipe) {
     const recipeContainer = document.getElementById('recipeContainer');
+    recipeContainer.innerHTML='';
 
     if (!recipe) {
       recipeContainer.innerHTML = '<p>No recipe found.</p>';
@@ -70,8 +78,9 @@ function searchRecipe() {
       }
     }
     const recipeInstructions = document.createElement('p');
-    recipeInstructions.innerHTML = recipe.strInstructions;
-
+    var txt = "<h3>Instructions: </h3>";
+    var link = `<p><a href=${recipe.strYoutube}><button id='inst'>Click here for video tutorial</button></a></p>`
+    recipeInstructions.innerHTML = txt + recipe.strInstructions + link;
     recipeContainer.appendChild(ingredientsList);
     recipeContainer.appendChild(recipeInstructions);
   }
@@ -82,6 +91,7 @@ function searchRecipe() {
     recipeButton.addEventListener('click',function(){
       displayRecipe(recipe);
       recipeButton.remove();
+      document.getElementById('error_text').remove();
     });
     recipeButtons.appendChild(recipeButton);
   }
